feae2a29e0c048cee27e84682953d14360762e27
Trishla Verma <trishla.verma@gmail.com>
2023-03-18 13:53:17 +0530

    Delete unused folders

--- java/CRUD.java
+++
@@ -1,4 +0,0 @@
-class CRUD {
-
-    create ()
-}
--- java/Location.java
+++
@@ -1,8 +0,0 @@
-public class Location {
-    Double latitude;
-    Double longitude;
-    public Location(Double latitude, Double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-}
